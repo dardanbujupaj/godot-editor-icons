28912bdba8e4afb35d8a427f0d5a7c71e4e5ca53
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-10-02 01:06:17 +0200

    add angular material. add virtual scroll for icon list. filter icon list items by searchtext. add dark background to better visualize icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,21 @@ import {HttpClientModule} from '@angular/common/http'
 import {FormsModule} from '@angular/forms'
 import {ReactiveFormsModule} from '@angular/forms'
 
+import {MatListModule} from '@angular/material/list'
+import {MatDividerModule} from '@angular/material/divider'
+import {MatSliderModule} from '@angular/material/slider'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button'
+import {MatMenuModule} from '@angular/material/menu'
+import {ScrollingModule} from '@angular/cdk/scrolling'
+
 import { AppComponent } from './app.component';
 import { IconBrowserComponent } from './icon-browser/icon-browser.component';
 import { IconListComponent } from './icon-list/icon-list.component';
 import { IconImageComponent } from './icon-image/icon-image.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,6 +31,17 @@ import { IconImageComponent } from './icon-image/icon-image.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatDividerModule,
+    MatSliderModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/icon-list/icon-list.component.ts
+++ src/app/icon-list/icon-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms'
+
+import {Observable, of} from 'rxjs'
+import {combineLatest, startWith, switchMap, tap} from 'rxjs/operators'
+
+import {IconMetadata} from '../icon-metadata'
 
 @Component({
   selector: 'app-icon-list',
@@ -7,13 +13,31 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class IconListComponent implements OnInit {
 
-  @Input() icons
+  @Input() icons: Observable<IconMetadata[]>
+  filteredIcons: Observable<IconMetadata[]>
 
-  size = 16
+
+  searchText = new FormControl('')
+
+  size = 32
 
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredIcons = this.icons.pipe(
+      // use latest icons and latest searchText (or emptyString if no searchText available)
+      combineLatest(
+        this.searchText
+              .valueChanges
+              .pipe(startWith(''))
+      ),
+
+      switchMap(([icons, searchText]) => {
+        const searchTextLower = searchText.toLowerCase()
+        const filtered = icons.filter(icon => icon.name.toLowerCase().includes(searchTextLower))
+        return of(filtered)
+      }),
+    )
   }
 
 }
